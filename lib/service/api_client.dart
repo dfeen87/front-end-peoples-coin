bbe9ace822f7d3b8331291c1e8ed5b2d0dab8e67
dfeen87 <dfeen87@gmail.com>
2025-07-23 19:19:16 -0400

    db connection

--- lib/service/api_client.dart
+++ lib/service/api_client.dart
@@ -14,7 +14,7 @@ import '../models/public_ledger_entry.dart';
 class PeoplesCoinApiClient {
   final String _baseUrl;
 
-  PeoplesCoinApiClient({String? baseUrl}) : _baseUrl = baseUrl ?? "http://10.0.2.2:5000";
+  PeoplesCoinApiClient({String? baseUrl}) : _baseUrl = baseUrl ?? "http://34.48.162.147:5000";
 
   /// Checks if the backend API is healthy and reachable.
   Future<bool> isApiHealthy() async {
